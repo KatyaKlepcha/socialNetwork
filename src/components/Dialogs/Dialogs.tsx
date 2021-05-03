0540f24efb0f384ee29111c02b116125766d7dbd
Katya <Solianchik@yandex.ru>
2021-05-03 23:04:40 +0300

    lesson 69,70: added a condition for checking the loginization

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent} from 'react'
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import { Redirect } from 'react-router-dom';
 
 type DialogsType = {
     id: number
@@ -17,11 +18,12 @@ type DialogsPageType = {
     newMessageBody: string
 }
 type DialogsPropsType = {
-    updateNewMessageBody: (body:string)=>void
+    updateNewMessageBody: (body: string) => void
     // state: AppStateType
     // dispatch: (action: ActionsTypes) => void
     dialogsPage: DialogsPageType
-    sendMessage: ()=> void
+    sendMessage: () => void
+    isAuth: boolean
 }
 const Dialogs = (props: DialogsPropsType) => {
     // let state = props.dialogsPage
@@ -29,7 +31,7 @@ const Dialogs = (props: DialogsPropsType) => {
     let dialogsElements = props.dialogsPage.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id}/>)
     // придет объект message
     let messagesElement = props.dialogsPage.messages.map(m => <Message message={m.message} key={m.id}/>)
-    let newMessageBody =  props.dialogsPage.newMessageBody;
+    let newMessageBody = props.dialogsPage.newMessageBody;
 
     // let newMassageElement = React.createRef<HTMLTextAreaElement>()
 
@@ -43,28 +45,30 @@ const Dialogs = (props: DialogsPropsType) => {
         //props.dispatch(updateNewMessageBodyCreator(body))
     }
 
+    if (!props.isAuth) return <Redirect to={'/login'}/>
 
-    return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogsElements}
-            </div>
-            <div className={s.messages}>
-                <div>{messagesElement}</div>
-                <div>
+
+        return (
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    {dialogsElements}
+                </div>
+                <div className={s.messages}>
+                    <div>{messagesElement}</div>
                     <div>
+                        <div>
                         <textarea value={newMessageBody}
                                   onChange={onNewMessageChange}//когда мы что-то вводим, срабатывает событие onChange
                                   placeholder='Enter your message'
                         />
-                    </div>
-                    <div>
-                        <button onClick={onSendMessageClick}>Send message</button>
+                        </div>
+                        <div>
+                            <button onClick={onSendMessageClick}>Send message</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
 
 export default Dialogs
