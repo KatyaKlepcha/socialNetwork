2f868d1bf0b0bca1da2f6ac7a931d6ad4b682097
Katya <Solianchik@yandex.ru>
2021-05-04 16:56:34 +0300

    lesson 71: created new file ProfileStatus

---
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+type ProfileStatusPropsType = {
+    status: string;
+}
+
+class ProfileStatus extends React.Component<ProfileStatusPropsType> {
+    state = {
+        editMode: false
+    }
+
+    activateEditMode = () => {
+        this.setState({
+                editMode: true
+            }
+        )
+    }
+    deActivateEditMode = () => {
+        this.setState({
+                editMode: false
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.editMode &&
+                <div>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                </div>
+                }
+                {this.state.editMode &&
+                <div>
+                    <input autoFocus={true} onBlur={this.deActivateEditMode} value={this.props.status}/>
+                </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
