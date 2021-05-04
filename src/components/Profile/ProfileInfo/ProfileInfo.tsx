b7e82d5809d98ad8946c1d781bf59e08ab67acd9
Katya <Solianchik@yandex.ru>
2021-05-04 16:57:15 +0300

    lesson 71: added new Component <ProfileStatus/>

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
 import {ProfileType} from "../../../redux/profile-reducer";
+import ProfileStatus from './ProfileStatus'
 
 type ProfileInfoPropsType = {
     profile: ProfileType
@@ -12,12 +13,12 @@ const ProfileInfo = (props: ProfileInfoPropsType) => {
     return (
         <div>
 
-            <div>
-                <img src='https://www.selltoearn.com/2018/norway-pier-dock-landscape120419.jpg' alt={'avatar'}/>
-            </div>
+            {/*<div>*/}
+            {/*    <img src='https://www.selltoearn.com/2018/norway-pier-dock-landscape120419.jpg' alt={'avatar'}/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src = {props.profile.photos?.large} alt={'ava'}/>
-                ava+description
+                <ProfileStatus status={'Hello my friends'}/>
             </div>
             <h1>{props.profile.fullName}</h1>
             <div>{props.profile.lookingForAJob}</div>
