a6957b481422ec7a562fab60e22e711810e717dd
Katya <Solianchik@yandex.ru>
2021-05-05 19:08:19 +0300

    lesson 73: added attribute updateStatus to <ProfileStatus />

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -5,9 +5,11 @@ import ProfileStatus from './ProfileStatus'
 
 type ProfileInfoPropsType = {
     profile: ProfileType
+    status: string
+    updateStatus: (status: string) => void
 }
 const ProfileInfo = (props: ProfileInfoPropsType) => {
-    if (!props.profile){
+    if (!props.profile) {
         return <Preloader/>
     }
     return (
@@ -17,12 +19,12 @@ const ProfileInfo = (props: ProfileInfoPropsType) => {
             {/*    <img src='https://www.selltoearn.com/2018/norway-pier-dock-landscape120419.jpg' alt={'avatar'}/>*/}
             {/*</div>*/}
             <div className={s.descriptionBlock}>
-                <img src = {props.profile.photos?.large} alt={'ava'}/>
-                <ProfileStatus status={'Hello my friends'}/>
+                <img src={props.profile.photos?.large} alt={'ava'}/>
+                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
             </div>
             <h1>{props.profile.fullName}</h1>
             <div>{props.profile.lookingForAJob}</div>
-            
+
         </div>
     )
 }
